389eda26f66ee26c931911ad8e41faa92ff9fbde
taehongkim <public.bluemiv@gmail.com>
2024-05-08 14:26:58 +0900

    feat: counter 앱 기능 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod_example/route/app_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,47 @@
 import "package:auto_route/annotations.dart";
 import "package:flutter/material.dart";
+import "package:flutter_riverpod_example/provider/counter_provider.dart";
+import "package:hooks_riverpod/hooks_riverpod.dart";
 
 @RoutePage()
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends HookConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int count = ref.watch(counterProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Riverpod 예제"),
+      ),
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            children: [
+              Text("count: $count"),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      ref.read(counterProvider.notifier).increment();
+                    },
+                    child: const Icon(Icons.plus_one),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      ref.read(counterProvider.notifier).decrement();
+                    },
+                    child: const Icon(Icons.exposure_minus_1),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
